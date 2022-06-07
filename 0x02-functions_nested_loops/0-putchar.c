3f256df0000c78ebe4f653f1f819623f8202367e
Imoudumicheal <imouduevelyn@gmail.com>
2022-06-07 13:42:15 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,16 @@
  * main - prints '_putchar'
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	
-	unsigned int i;
-	for (i-0; i < sizeof(word); i++)
+	int str[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar(word[i]);
+		_putchar(str[count]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
