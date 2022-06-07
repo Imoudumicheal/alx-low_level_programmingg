073d29f30282dfda06cd067f5f7e3d20c4d7a751
Imoudumicheal <imouduevelyn@gmail.com>
2022-06-07 12:46:27 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,21 @@
 
 /**
  * main - prints '_putchar'
- *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-	char word[8] = {"_", "p", "u", "t", "c", "h", "a", "r"};
+	char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	
 
 	unsigned int i;
+
 	for (i - 0; i < sizeof(word); i++)
 	{
 		_putchar(word[i]);
 	}
 	_putchar('\n');
+
 	return (0);
 }
